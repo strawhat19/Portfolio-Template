ca92784a0fe3da125c33e709577fb51fcce0f5af
rakib <rahmed@hvac.mea.com>
2023-10-15 16:39:18 -0400

    Changes to Home

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
+const years = $(`.year`);
 const lastUpdated = $(`.lastUpdated`);
 lastUpdated.html(`${dayjs().$M + 1} - ${dayjs().$D} - ${dayjs().$y}`);
+years.each((index, element) => {
+    $(element).html(dayjs().$y);
+});
