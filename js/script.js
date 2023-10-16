fb6cf310fc007fe01510c237f79b9b2d6e8eacbb
rakib <rahmed@hvac.mea.com>
2023-10-16 11:57:23 -0400

    PIR-892, PIR-895: Home Page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const years = $(`.year`);
 const lastUpdated = $(`.lastUpdated`);
+
 lastUpdated.html(`${dayjs().$M + 1} - ${dayjs().$D} - ${dayjs().$y}`);
-years.each((index, element) => {
-    $(element).html(dayjs().$y);
+years.each((yearIndex, yearElement) => {
+    $(yearElement).html(dayjs().$y);
 });
